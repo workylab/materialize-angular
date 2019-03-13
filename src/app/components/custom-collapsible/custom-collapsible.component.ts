5260e4c29bf4a07e862bb1ab860aec3f73c3f449
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-03-13 00:11:52 -0400

    Added onclick and onresize window events support to collapsible component.

--- src/app/components/custom-collapsible/custom-collapsible.component.ts
+++ src/app/components/custom-collapsible/custom-collapsible.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Renderer2, Output, EventEmitter, SimpleChanges } from '@angular/core';
 import { CustomCollapsible } from './custom-collapsible.model';
+import { getBooleanValue } from '../../utils/get-boolean-value.util';
 
 @Component({
   selector: 'custom-collapsible',
@@ -14,10 +15,13 @@ export class CustomCollapsibleComponent implements CustomCollapsible, OnInit {
     title: ''
   };
 
+  @Output('onClick') onClickEventEmitter: EventEmitter<boolean>;
+
   @Input('className') classNameInput: string;
   @Input('content') contentInput: string;
   @Input('iconName') iconNameInput: string;
   @Input('title') titleInput: string;
+  @Input('isOpen') isOpenInput: boolean;
 
   public className: string;
   public content: string;
@@ -26,10 +30,23 @@ export class CustomCollapsibleComponent implements CustomCollapsible, OnInit {
   public title: string;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {
+    this.onClickEventEmitter = new EventEmitter();
+    this.onResizeWindow = this.onResizeWindow.bind(this);
+
+    window.addEventListener('resize', this.onResizeWindow);
   }
 
   ngOnInit() {
     this.initValues();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const { isOpenInput } = changes;
+
+    if (isOpenInput && isOpenInput.currentValue !== isOpenInput.previousValue) {
+      this.isOpen = isOpenInput.currentValue;
+      this.toggleCollapsible(this.isOpen);
+    }
   }
 
   initValues() {
@@ -38,20 +55,31 @@ export class CustomCollapsibleComponent implements CustomCollapsible, OnInit {
     this.className = this.classNameInput || defaultProps.className;
     this.content = this.contentInput || defaultProps.content;
     this.iconName = this.iconNameInput || defaultProps.iconName;
-    this.isOpen = defaultProps.isOpen;
+    this.isOpen = getBooleanValue(this.isOpenInput, defaultProps.isOpen);
     this.title = this.titleInput || defaultProps.title;
   }
 
-  toggle() {
+  onResizeWindow() {
+    if (this.isOpen) {
+      this.toggleCollapsible(this.isOpen);
+    }
+  }
+
+  onToggle() {
+    this.isOpen = !this.isOpen;
+
+    this.toggleCollapsible(this.isOpen);
+    this.onClickEventEmitter.emit(this.isOpen);
+  }
+
+  toggleCollapsible(isOpen: boolean) {
     const { children } = this.element.nativeElement.firstChild;
     const contentContainer: HTMLElement = children[children.length - 1];
 
-    if (contentContainer.style.maxHeight) {
-      this.renderer.setStyle(contentContainer, 'maxHeight', null);
-      this.isOpen = false;
-    } else {
-      this.isOpen = true;
+    if (isOpen) {
       this.renderer.setStyle(contentContainer, 'maxHeight', `${ contentContainer.scrollHeight }px`);
+    } else {
+      this.renderer.setStyle(contentContainer, 'maxHeight', null);
     }
   }
 }
