91f9be17ca7b578e836ea5ef454c0d652b2dedf8
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-03-13 00:10:04 -0400

    Added accordion model.

---
+++ src/app/components/custom-accordion/custom-accordion.model.ts
@@ -0,0 +1,6 @@
+import { CustomCollapsible } from '../custom-collapsible/custom-collapsible.model';
+
+export interface CustomAccordion {
+  className: string;
+  items: Array<CustomCollapsible>;
+}
