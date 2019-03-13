b11215449611b7bc9f0df85d55c2c1b4f77a8549
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-03-12 20:09:42 -0400

    Added custom button toggle component.

---
+++ src/app/components/custom-button-toggle/custom-button-toggle.component.ts
@@ -0,0 +1,86 @@
+import { ButtonToggle, ButtonToggleItem } from './custom-button-toggle.model';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormField } from '../custom-form/custom-form.model';
+import { getBooleanValue } from '../../utils/get-boolean-value.util';
+
+@Component({
+  selector: 'custom-button-toggle',
+  templateUrl: './custom-button-toggle.component.html'
+})
+export class CustomButtonToggleComponent implements ButtonToggle, FormField, OnInit {
+  static defaultProps: ButtonToggle = {
+    className: '',
+    disabled: false,
+    isFocused: false,
+    isMultiple: false,
+    isTouched: false,
+    isValid: false,
+    items: [],
+    label: '',
+    name: '',
+    required: false,
+    value: []
+  };
+
+  @Output('onClick') onClickEmitter: EventEmitter<string>;
+
+  @Input('className') classNameInput: string;
+  @Input('disabled') disabledInput: boolean;
+  @Input('items') itemsInput: Array<ButtonToggleItem>;
+  @Input('isMultiple') isMultipleInput: boolean;
+  @Input('label') labelInput: string;
+  @Input('name') nameInput: string;
+  @Input('required') requiredInput: boolean;
+  @Input('value') valueInput: Array<string>;
+
+  public className: string;
+  public disabled: boolean;
+  public isFocused: boolean;
+  public isMultiple: boolean;
+  public isTouched: boolean;
+  public isValid: boolean;
+  public items: Array<ButtonToggleItem>;
+  public label: string;
+  public name: string;
+  public required: boolean;
+  public value: Array<string>;
+
+  constructor() {
+    this.onClickEmitter = new EventEmitter();
+  }
+
+  ngOnInit() {
+    this.initValues();
+  }
+
+  initValues() {
+    const { defaultProps } = CustomButtonToggleComponent;
+
+    this.className = this.classNameInput || defaultProps.className;
+    this.disabled = getBooleanValue(this.disabledInput, defaultProps.disabled);
+    this.isMultiple = getBooleanValue(this.isMultipleInput, defaultProps.isMultiple);
+    this.items = this.itemsInput || defaultProps.items;
+    this.label = this.label || defaultProps.label;
+    this.name = this.nameInput || defaultProps.name;
+    this.required = getBooleanValue(this.required, defaultProps.required);
+    this.value = this.valueInput || defaultProps.value;
+
+    this.isFocused = defaultProps.isFocused;
+    this.isTouched = defaultProps.isTouched;
+    this.isValid = defaultProps.isValid;
+  }
+
+  onClick(item: ButtonToggleItem) {
+    if (!this.disabled) {
+      const valueIndex = this.value.indexOf(item.value);
+
+      if (valueIndex >= 0) {
+        this.value.splice(valueIndex, 1);
+      } else {
+        this.value.push(item.value);
+      }
+
+      this.onClickEmitter.emit(item.value);
+    }
+  }
+}
