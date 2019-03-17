7c37841e3087a0246288e0e63608f50d47eca019
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-03-16 21:45:46 -0400

    Added class name to swiper item.

--- src/app/components/custom-swiper/custom-swiper-item/custom-swiper-item.component.ts
+++ src/app/components/custom-swiper/custom-swiper-item/custom-swiper-item.component.ts
@@ -1,11 +1,29 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'custom-swiper-item',
   templateUrl: './custom-swiper-item.component.html'
 })
-export class CustomSwiperItemComponent {
+export class CustomSwiperItemComponent implements OnInit {
+  static readonly defaultProps = {
+    className: ''
+  };
+
   @ViewChild('swiperItemContainer') swiperItemContainerRef: ElementRef;
+
+  @Input('className') classNameInput: string;
+
+  public className: string;
+
+  ngOnInit() {
+    this.initValues();
+  }
+
+  initValues() {
+    const { defaultProps } = CustomSwiperItemComponent;
+
+    this.className = this.classNameInput || defaultProps.className;
+  }
 
   get swiperItemContainer(): HTMLElement {
     return this.swiperItemContainerRef.nativeElement;
