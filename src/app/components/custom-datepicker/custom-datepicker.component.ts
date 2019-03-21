724ce3427aad3f1c3509cb15475c3938e60d5758
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-03-20 23:13:39 -0400

    Added datepicker simple implementation.

--- src/app/components/custom-datepicker/custom-datepicker.component.ts
+++ src/app/components/custom-datepicker/custom-datepicker.component.ts
@@ -1,21 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CustomCalendarDay } from '../custom-calendar/custom-calendar.model';
 import { CustomDatePicker } from './custom-datepicker.model';
+import { getBooleanValue } from '../../utils/get-boolean-value.util';
 
 @Component({
   selector: 'custom-datepicker',
   templateUrl: './custom-datepicker.component.html'
 })
-export class CustomDatePickerComponent implements CustomDatePicker {
+export class CustomDatePickerComponent implements CustomDatePicker, OnInit {
+  static readonly defaultProps: CustomDatePicker = {
+    className: '',
+    disabled: false,
+    floatLabel: true,
+    iconName: '',
+    id: '',
+    isFocused: false,
+    isTouched: false,
+    isValid: false,
+    label: '',
+    name: '',
+    required: false,
+    value: ''
+  };
+
   @Input('className') classNameInput: string;
+  @Input('disabled') disabledInput: boolean;
+  @Input('floatLabel') floatLabelInput: boolean;
   @Input('iconName') iconNameInput: string;
   @Input('id') idInput: string;
   @Input('label') labelInput: string;
   @Input('name') nameInput: string;
+  @Input('required') requiredInput: boolean;
   @Input('value') valueInput: string;
 
   public className: string;
   public disabled: boolean;
+  public floatLabel: boolean;
   public iconName: string;
   public id: string;
   public isFocused: boolean;
@@ -28,6 +48,28 @@ export class CustomDatePickerComponent implements CustomDatePicker {
 
   constructor() {
     this.onInputBlur = this.onInputBlur.bind(this);
+  }
+
+  ngOnInit() {
+    this.initValues();
+  }
+
+  initValues() {
+    const { defaultProps } = CustomDatePickerComponent;
+
+    this.className = this.classNameInput || defaultProps.className;
+    this.disabled = getBooleanValue(this.disabledInput, defaultProps.disabled);
+    this.floatLabel = getBooleanValue(this.floatLabelInput, defaultProps.floatLabel);
+    this.iconName = this.iconNameInput || defaultProps.iconName;
+    this.id = this.idInput || defaultProps.id;
+    this.label = this.labelInput || defaultProps.label;
+    this.name = this.nameInput || defaultProps.name;
+    this.required = getBooleanValue(this.requiredInput, defaultProps.required);
+    this.value = this.valueInput || defaultProps.value;
+
+    this.isFocused = defaultProps.isFocused;
+    this.isTouched = defaultProps.isTouched;
+    this.isValid = defaultProps.isValid;
   }
 
   onSelectDay(day: CustomCalendarDay) {
