a693ec2fef4738538f42c00e617bea586c181e65
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-04-04 23:03:17 -0400

    Added form field class base to checkbox component.

--- src/app/components/custom-checkbox/custom-checkbox.model.ts
+++ src/app/components/custom-checkbox/custom-checkbox.model.ts
@@ -1,6 +1,6 @@
-import { FormField } from '../custom-form/custom-form.model';
+import { CustomFormFieldAbstract } from '../custom-form/custom-form-field.abstract';
 
-export interface CustomCheckbox extends FormField {
+export interface CustomCheckbox extends CustomFormFieldAbstract {
   iconName: string;
   indeterminate: boolean;
   value: boolean;
