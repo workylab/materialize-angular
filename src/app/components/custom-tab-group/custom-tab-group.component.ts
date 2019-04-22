9d718857e47ebb44d792e00adb886fee4d2f5311
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-04-21 22:12:30 -0400

    Improved custom tab component.

--- src/app/components/custom-tab-group/custom-tab-group.component.ts
+++ src/app/components/custom-tab-group/custom-tab-group.component.ts
@@ -1,26 +1,64 @@
-import { Component, ContentChildren, QueryList } from '@angular/core';
+import { AfterContentInit, Component, ContentChildren, ElementRef, Input, QueryList, ViewChild } from '@angular/core';
 import { CustomTabComponent } from '../custom-tab/custom-tab.component';
+import { CustomTabGroup } from './custom-tab-group.model';
 
 @Component({
   selector: 'custom-tab-group',
   templateUrl: './custom-tab-group.component.html'
 })
-export class CustomTabGroupComponent {
+export class CustomTabGroupComponent implements AfterContentInit {
+  static readonly defaultProps: CustomTabGroup = {
+    className: '',
+    selectedIndexInput: 0
+  };
+
   @ContentChildren(CustomTabComponent) tabComponentList: QueryList<CustomTabComponent>;
 
-  ngAfterContentInit() {
-    const activeTabs = this.tabComponentList.filter(item => item.isActive);
+  @ViewChild('tabsIndicator') tabsIndicatorRef: ElementRef;
+  @ViewChild('tabsHeader') tabsHeader: ElementRef;
 
-    if (activeTabs.length === 0) {
-      this.selectTab(this.tabComponentList.first);
-    }
+  @Input('className') classNameInput: string;
+  @Input('selectedIndex') selectedIndexInput: number;
+
+  public className: string;
+  public selectedIndex: number;
+
+  constructor() {
+    this.initValues = this.initValues.bind(this);
   }
 
-  selectTab(tabComponent: CustomTabComponent) {
-    this.tabComponentList.toArray().forEach(item => {
-      item.isActive = false;
-    });
+  ngAfterContentInit() {
+    setTimeout(this.initValues, 500);
+  }
 
-    tabComponent.isActive = true;
+  initValues() {
+    const { defaultProps } = CustomTabGroupComponent;
+
+    this.className = this.classNameInput || defaultProps.className;
+    this.selectedIndex = this.selectedIndexInput || defaultProps.selectedIndexInput;
+
+    this.selectTab(this.selectedIndex);
+  }
+
+  selectTab(newIndex: number) {
+    this.selectedIndex = newIndex;
+
+    this.activateIndex(this.selectedIndex);
+
+    const child = this.tabsHeader.nativeElement.children[this.selectedIndex];
+    const { style } = this.tabsIndicatorRef.nativeElement;
+
+    style.width = `${ child.offsetWidth }px`;
+    style.transform = `translateX(${ child.offsetLeft }px)`;
+  }
+
+  activateIndex(index: number) {
+    const tabList = this.tabComponentList.toArray();
+
+    for (const tab of tabList) {
+      tab.isActive = false;
+    }
+
+    tabList[index].isActive = true;
   }
 }
--- src/app/components/custom-tab-group/custom-tab-group.model.ts
+++ src/app/components/custom-tab-group/custom-tab-group.model.ts
@@ -1,3 +1,4 @@
 export interface CustomTabGroup {
   className: string;
+  selectedIndexInput: number;
 }
