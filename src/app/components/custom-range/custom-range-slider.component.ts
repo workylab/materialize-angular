b57363a415fdfbd04c6af114c0e50cb7c12225ab
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-04-23 13:33:19 -0400

    Added range slider component.

---
+++ src/app/components/custom-range/custom-range-slider.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './custom-range-slider.component.html',
+  selector: 'custom-range-slider'
+})
+export class CustomRangeSliderComponent {
+
+}
