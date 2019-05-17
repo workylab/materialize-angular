50bfc9c8cf67748849c817fbbe69231f159e2c3c
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-05-17 16:43:12 -0400

    Removed label and error message from form base class.

--- src/app/components/form/form-field.abstract.ts
+++ src/app/components/form/form-field.abstract.ts
@@ -3,12 +3,10 @@ import { FileModel } from '../input-file/input-file.model';
 export abstract class FormFieldAbstract {
   public className: string;
   public disabled: boolean;
-  public errorMessage: string;
   public id: string | null;
   public isFocused: boolean;
   public isTouched: boolean;
   public isValid: boolean;
-  public label: string;
   public name: string;
   public required: boolean;
   public value: string | boolean | number | Array<FileModel> | { [key: string]: boolean | string; };
