2b9686b759ec029921f48639e04a911ce5c2f0ac
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-06-02 22:33:01 -0400

    Added calendar month texts.

--- src/app/fixtures/calendar-months.ts
+++ src/app/fixtures/calendar-months.ts
@@ -1,14 +1,39 @@
-export default {
-  april: 'APR',
-  august: 'AUG',
-  december: 'DEC',
-  february: 'FEB',
-  january: 'JAN',
-  july: 'JUL',
-  june: 'JUN',
-  march: 'MAR',
-  may: 'MAY',
-  november: 'NOV',
-  october: 'OCT',
-  september: 'SEP'
-};
+import { Month } from '../components/calendar/calendar.model';
+
+export const months: Array<Month> = [{
+  name: 'January',
+  shortName: 'Jan'
+}, {
+  name: 'February',
+  shortName: 'Feb'
+}, {
+  name: 'March',
+  shortName: 'Mar'
+}, {
+  name: 'April',
+  shortName: 'Apr'
+}, {
+  name: 'May',
+  shortName: 'May'
+}, {
+  name: 'June',
+  shortName: 'Jun'
+}, {
+  name: 'July',
+  shortName: 'Jul'
+}, {
+  name: 'August',
+  shortName: 'Aug'
+}, {
+  name: 'September',
+  shortName: 'Sep'
+}, {
+  name: 'October',
+  shortName: 'Oct'
+}, {
+  name: 'November',
+  shortName: 'Nov'
+}, {
+  name: 'December',
+  shortName: 'Dec'
+}];
