53c36c732edb7890d06a6a25bf17c39af01c3b07
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-06-03 21:35:19 -0400

    Updated week day labels.

--- src/app/fixtures/calendar-week-days.ts
+++ src/app/fixtures/calendar-week-days.ts
@@ -1,9 +1,31 @@
-export default {
-  friday: 'F',
-  monday: 'M',
-  saturday: 'S',
-  sunday: 'S',
-  thursday: 'T',
-  tuesday: 'T',
-  wednesday: 'W'
-};
+import { DayModel } from '../components/calendar/calendar.model';
+
+export const days: Array<DayModel> = [{
+  name: 'Sunday',
+  shortName: 'Sun',
+  shortestName: 'Su'
+}, {
+  name: 'Monday',
+  shortName: 'Mon',
+  shortestName: 'Mo'
+}, {
+  name: 'Tuesday',
+  shortName: 'Tue',
+  shortestName: 'Tu'
+}, {
+  name: 'Wednesday',
+  shortName: 'Wed',
+  shortestName: 'We'
+}, {
+  name: 'Thursday',
+  shortName: 'Thu',
+  shortestName: 'Th'
+}, {
+  name: 'Friday',
+  shortName: 'Fri',
+  shortestName: 'Fr'
+}, {
+  name: 'Saturday',
+  shortName: 'Sat',
+  shortestName: 'Sa'
+}];
