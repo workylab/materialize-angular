d12d62dd41bbf22596767a01b4fb39556cc0813a
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-06-03 21:36:03 -0400

    Update calendar models.

--- src/app/components/calendar/calendar.model.ts
+++ src/app/components/calendar/calendar.model.ts
@@ -1,6 +1,27 @@
+export interface CalendarModel {
+  displayOtherMonthDays: boolean;
+}
+
 export interface CalendarDayModel {
-  dayNumber: number;
+  numberInMonth: number;
   isCurrent: boolean;
   isOutOfMonth: boolean;
-  isoDate: string;
+  ISODate: string;
+}
+
+export interface DayModel {
+  name: string;
+  shortName: string;
+  shortestName: string;
+}
+
+export interface MonthModel {
+  name: string;
+  shortName: string;
+}
+
+export interface DateModel {
+  day: number;
+  month: number;
+  year: number;
 }
