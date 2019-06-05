0db170d2dce93de4630003782dd7f1da7977752d
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-06-05 16:10:56 -0400

    Added more options to display months.

--- src/app/fixtures/calendar-months.ts
+++ src/app/fixtures/calendar-months.ts
@@ -1,39 +1,64 @@
-import { MonthModel } from '../components/calendar/calendar.model';
+import { MonthLabels } from '../components/calendar/calendar.model';
 
-export const months: Array<MonthModel> = [{
-  name: 'January',
-  shortName: 'Jan'
-}, {
-  name: 'February',
-  shortName: 'Feb'
-}, {
-  name: 'March',
-  shortName: 'Mar'
-}, {
-  name: 'April',
-  shortName: 'Apr'
-}, {
-  name: 'May',
-  shortName: 'May'
-}, {
-  name: 'June',
-  shortName: 'Jun'
-}, {
-  name: 'July',
-  shortName: 'Jul'
-}, {
-  name: 'August',
-  shortName: 'Aug'
-}, {
-  name: 'September',
-  shortName: 'Sep'
-}, {
-  name: 'October',
-  shortName: 'Oct'
-}, {
-  name: 'November',
-  shortName: 'Nov'
-}, {
-  name: 'December',
-  shortName: 'Dec'
-}];
+export const months: MonthLabels = {
+  april: {
+    name: 'April',
+    shortName: 'Apr',
+    shortestName: 'Ap'
+  },
+  august: {
+    name: 'August',
+    shortName: 'Aug',
+    shortestName: 'Au'
+  },
+  december: {
+    name: 'December',
+    shortName: 'Dec',
+    shortestName: 'De'
+  },
+  february: {
+    name: 'February',
+    shortName: 'Feb',
+    shortestName: 'Fe'
+  },
+  january: {
+    name: 'January',
+    shortName: 'Jan',
+    shortestName: 'Ja'
+  },
+  july: {
+    name: 'July',
+    shortName: 'Jul',
+    shortestName: 'Ju'
+  },
+  june: {
+    name: 'June',
+    shortName: 'Jun',
+    shortestName: 'Ju'
+  },
+  march: {
+    name: 'March',
+    shortName: 'Mar',
+    shortestName: 'Ma'
+  },
+  may: {
+    name: 'May',
+    shortName: 'May',
+    shortestName: 'Ma'
+  },
+  november: {
+    name: 'November',
+    shortName: 'Nov',
+    shortestName: 'No'
+  },
+  october: {
+    name: 'October',
+    shortName: 'Oct',
+    shortestName: 'Oc'
+  },
+  september: {
+    name: 'September',
+    shortName: 'Sep',
+    shortestName: 'Se'
+  }
+};
