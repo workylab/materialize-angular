47a18af0714a9f327e6f1807075388d24eff565d
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-06-05 16:11:11 -0400

    Added more options to display day labels.

--- src/app/fixtures/calendar-week-days.ts
+++ src/app/fixtures/calendar-week-days.ts
@@ -1,31 +1,39 @@
-import { DayModel } from '../components/calendar/calendar.model';
+import { DayLabels } from '../components/calendar/calendar.model';
 
-export const days: Array<DayModel> = [{
-  name: 'Sunday',
-  shortName: 'Sun',
-  shortestName: 'Su'
-}, {
-  name: 'Monday',
-  shortName: 'Mon',
-  shortestName: 'Mo'
-}, {
-  name: 'Tuesday',
-  shortName: 'Tue',
-  shortestName: 'Tu'
-}, {
-  name: 'Wednesday',
-  shortName: 'Wed',
-  shortestName: 'We'
-}, {
-  name: 'Thursday',
-  shortName: 'Thu',
-  shortestName: 'Th'
-}, {
-  name: 'Friday',
-  shortName: 'Fri',
-  shortestName: 'Fr'
-}, {
-  name: 'Saturday',
-  shortName: 'Sat',
-  shortestName: 'Sa'
-}];
+export const days: DayLabels = {
+  friday: {
+    name: 'Friday',
+    shortName: 'Fri',
+    shortestName: 'Fr'
+  },
+  monday: {
+    name: 'Monday',
+    shortName: 'Mon',
+    shortestName: 'Mo'
+  },
+  saturday: {
+    name: 'Saturday',
+    shortName: 'Sat',
+    shortestName: 'Sa'
+  },
+  sunday: {
+    name: 'Sunday',
+    shortName: 'Sun',
+    shortestName: 'Su'
+  },
+  thursday: {
+    name: 'Thursday',
+    shortName: 'Thu',
+    shortestName: 'Th'
+  },
+  tuesday: {
+    name: 'Tuesday',
+    shortName: 'Tue',
+    shortestName: 'Tu'
+  },
+  wednesday: {
+    name: 'Wednesday',
+    shortName: 'Wed',
+    shortestName: 'We'
+  }
+};
