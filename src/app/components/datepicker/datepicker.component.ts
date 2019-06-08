e460c1fd0e0f2ee84617e62a7ba0fd41edba7937
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-06-07 22:16:17 -0400

    Added datepicker component and funcionality to set input value from calendar component.

--- src/app/components/datepicker/datepicker.component.ts
+++ src/app/components/datepicker/datepicker.component.ts
@@ -1,4 +1,4 @@
-import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, forwardRef, Input, OnInit, ViewChild } from '@angular/core';
 import { DateModel } from '../calendar/calendar.model';
 import { DatePickerModel } from './datepicker.model';
 import { FormFieldAbstract } from '../form/form-field.abstract';
@@ -19,6 +19,8 @@ export class DatePickerComponent extends FormFieldAbstract implements OnInit {
     className: '',
     disabled: false,
     floatLabel: '',
+    format: 'dd-mm-yyyy',
+    fullSize: false,
     hasCounter: false,
     id: null,
     maxLength: 500,
@@ -26,35 +28,38 @@ export class DatePickerComponent extends FormFieldAbstract implements OnInit {
     placeholder: '',
     required: false,
     type: 'text',
-    validateOnBlur: false,
-    validateOnChange: false,
     value: ''
   };
+
+  @ViewChild('backdrop') backdropRef: ElementRef;
 
   @Input('className') classNameInput: string;
   @Input('disabled') disabledInput: boolean;
   @Input('floatLabel') floatLabelInput: string;
+  @Input('format') formatInput: string;
+  @Input('fullSize') fullSizeInput: boolean;
   @Input('id') idInput: string | null;
   @Input('name') nameInput: string;
+  @Input('placeholder') placeholderInput: string;
   @Input('required') requiredInput: boolean;
   @Input('value') valueInput: string;
 
   public className: string;
   public disabled: boolean;
-  public errorMessage: string;
   public floatLabel: string;
+  public format: string;
+  public fullSize: boolean;
   public id: string | null;
-  public isFocused: boolean;
-  public isTouched: boolean;
-  public isValid: boolean;
+  public isOpen: boolean;
   public name: string;
+  public placeholder: string;
   public required: boolean;
   public value: string;
 
   constructor() {
     super();
 
-    this.onInputBlur = this.onInputBlur.bind(this);
+    this.close = this.close.bind(this);
   }
 
   ngOnInit() {
@@ -67,40 +72,53 @@ export class DatePickerComponent extends FormFieldAbstract implements OnInit {
     this.className = this.classNameInput || defaultProps.className;
     this.disabled = getBooleanValue(this.disabledInput, defaultProps.disabled);
     this.floatLabel = this.floatLabelInput || defaultProps.floatLabel;
+    this.format = this.formatInput || defaultProps.format;
+    this.fullSize = getBooleanValue(this.fullSizeInput, defaultProps.fullSize);
     this.id = this.idInput || defaultProps.id;
     this.name = this.nameInput || defaultProps.name;
+    this.placeholder = this.placeholderInput || defaultProps.placeholder;
     this.required = getBooleanValue(this.requiredInput, defaultProps.required);
     this.value = this.valueInput || defaultProps.value;
 
-    this.isFocused = false;
-    this.isTouched = false;
-    this.isValid = false;
+    this.isOpen = false;
   }
 
-  onSelectDay(day: DateModel) {
-    this.value = day.ISODate;
-    this.isFocused = false;
+  onSelectDay(selectedDate: DateModel) {
+    this.value = this.formatDate(selectedDate.date);
+    this.isOpen = false;
   }
 
-  onInputFocus() {
-    this.isFocused = true;
+  formatDate(date: Date) {
+    const day = date.getDate();
+    const month = date.getMonth();
+
+    const dayString = day < 10
+      ? `0${ day }`
+      : day.toString();
+
+    const monthString = month < 10
+      ? `0${ month }`
+      : month.toString();
+
+    const yearString = date.getFullYear().toString();
+
+    const formatedDate = this.format
+      .replace('dd', dayString)
+      .replace('mm', monthString)
+      .replace('yyyy', yearString);
+
+    return formatedDate;
   }
 
-  onInputBlur(event: any) {
-    const { relatedTarget } = event;
+  open() {
+    this.isOpen = true;
 
-    if (!relatedTarget || relatedTarget.className !== 'calendar-container') {
-      this.isFocused = false;
-    }
+    setTimeout(() => {
+      this.backdropRef.nativeElement.addEventListener('click', this.close);
+    }, 0);
   }
 
-  onCalendarBlur(event: Event) {
-    this.isFocused = false;
-  }
-
-  updateAndValidity() {
-    this.isTouched = true;
-
-    // TODO: this.isValid = this.validate(this.value, this.required);
+  close() {
+    this.isOpen = false;
   }
 }
