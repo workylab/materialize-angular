c48346bc0629eae0d1bc3d1dfd42431f750a25de
Alejandro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-06-09 21:44:41 -0400

    Improved date picker model.

--- src/app/components/datepicker/datepicker.model.ts
+++ src/app/components/datepicker/datepicker.model.ts
@@ -1,6 +1,16 @@
-import { InputModel } from '../input/input.model';
-
-export interface DatePickerModel extends InputModel {
+export interface DatePickerModel {
+  autocomplete: string;
+  className: string;
+  disabled: boolean;
+  floatLabel: string;
   format: string;
   fullSize: boolean;
+  id: string | null;
+  hasCounter: boolean;
+  name: string;
+  maxLength: number;
+  placeholder: string;
+  required: boolean;
+  type: string;
+  value: Date;
 }
