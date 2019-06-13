ec74c7d9eb874874048ea25766c6de94634cc976
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-06-13 19:16:39 -0400

    Added range slider basic model

--- src/app/components/range-slider/range-slider.model.ts
+++ src/app/components/range-slider/range-slider.model.ts
@@ -1,8 +1,11 @@
 export interface RangeSliderModel {
   className: string;
+  minValue: number;
+  maxValue: number;
+  value: number;
 }
 
-export interface RangeOptionModel {
+export interface RangeSliderOptionModel {
   label: string;
   value: number;
 }
