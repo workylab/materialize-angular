1259efb9fd9261b139ec24ae616f4566c9db1014
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-06-27 15:16:07 -0400

    Improved modal handler.

--- src/app/components/modal-handler/modal-handler.component.ts
+++ src/app/components/modal-handler/modal-handler.component.ts
@@ -1,10 +1,18 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Component({
   selector: 'materialize-modal-handler',
   templateUrl: './modal-handler.component.html'
 })
 export class ModalHandlerComponent {
+  @Output('onClick') onClickEmitter: EventEmitter<void>;
+
   constructor(public elementRef: ElementRef) {
+    this.onClickEmitter = new EventEmitter();
+  }
+
+  @HostListener('click')
+  onClick() {
+    this.onClickEmitter.emit();
   }
 }
