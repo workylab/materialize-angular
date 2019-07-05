b05efcb057623db8894ffc25a79184fc283448a5
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-07-04 23:56:10 -0400

    Fixxed navbar component.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { NavbarOptionModel } from './navbar.model';
-
-interface defaultProps {
-  className: string;
-  menuIconName: string;
-  options: Array<NavbarOptionModel>;
-  title: string;
-  titleIconName: string;
-}
+import { NavbarModel } from './navbar.model';
 
 @Component({
   selector: 'materialize-navbar',
@@ -15,25 +7,13 @@ interface defaultProps {
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
-  static readonly defaultProps: defaultProps = {
-    className: 'navbar',
-    menuIconName: 'menu',
-    options: [],
-    title: 'Navbar',
-    titleIconName: ''
+  static readonly defaultProps: NavbarModel = {
+    className: 'navbar'
   };
 
-  @Input() className: string;
-  @Input() menuIconName: string;
-  @Input() title: string;
-  @Input() titleIconName: string;
-  @Input() options: Array<NavbarOptionModel>;
+  @Input() classNameInput: string;
 
-  public _className: string;
-  public _menuIconName: string;
-  public _title: string;
-  public _titleIconName: string;
-  public _options: Array<NavbarOptionModel>
+  public className: string;
 
   ngOnInit() {
     this.initValues();
@@ -42,10 +22,6 @@ export class NavbarComponent implements OnInit {
   initValues() {
     const { defaultProps } = NavbarComponent;
 
-    this._className = this.className || defaultProps.className;
-    this._menuIconName = this.menuIconName || defaultProps.menuIconName;
-    this._options = this.options || defaultProps.options;
-    this._title = this.title || defaultProps.title;
-    this._titleIconName = this.titleIconName || defaultProps.titleIconName;
+    this.className = this.classNameInput || defaultProps.className;
   }
 }
