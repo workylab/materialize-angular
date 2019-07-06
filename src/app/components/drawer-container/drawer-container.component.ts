5f85395a5a32320b4e8f57f23791a2814dae9862
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-07-06 13:46:13 -0400

    Added drawer container component.

---
+++ src/app/components/drawer-container/drawer-container.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'materialize-drawer-container',
+  templateUrl: './drawer-container.component.html'
+})
+export class DrawerContainerComponent {
+}
