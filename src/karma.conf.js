29fe13aaa9780e2df3dcabbe67bb3ff32c19885c
Israel Gayoso Perez <igayoso@gmail.com>
2019-07-06 08:47:46 -0400

    Add karma config for circleci

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,8 +25,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      MyHeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--disable-translate', '--disable-extensions']
+      }
+    },    
+    singleRun: true,
     restartOnFileChange: true
   });
 };
