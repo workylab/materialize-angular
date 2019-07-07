e01637e94f4e22fcadf21ff999a7d1445b2443d2
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-07-07 03:48:49 -0400

    Improved drawer container component.

--- src/app/components/drawer-container/drawer-container.component.ts
+++ src/app/components/drawer-container/drawer-container.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'materialize-drawer-container',
   templateUrl: './drawer-container.component.html'
 })
 export class DrawerContainerComponent {
+  constructor(private elementRef: ElementRef) {
+  }
 }
