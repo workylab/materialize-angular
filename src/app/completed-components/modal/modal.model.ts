b622f4aa59e4a701a2095c9bc8d237ff3c55a884
Alejandro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-07-25 11:18:55 -0400

    Improved modal component.

---
+++ src/app/completed-components/modal/modal.model.ts
@@ -0,0 +1,8 @@
+export interface ModalModel {
+  className: string;
+  dismissOnBackdrop: boolean;
+  hasBackdrop: boolean;
+  hasCloseButton: boolean;
+  isOpen: boolean;
+  transitionDuration: number;
+}
