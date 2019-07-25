954b5ad257e0200e27a26495c18ec346a04123e2
Alejandro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-07-25 11:19:25 -0400

    Improved modal handler component.

---
+++ src/app/completed-components/modal-handler/modal-handler.component.ts
@@ -0,0 +1,18 @@
+import { Component, HostListener, Input } from '@angular/core';
+import { config } from '../../config';
+import { ModalComponent } from '../modal/modal.component';
+
+@Component({
+  selector: `${ config.components.prefix }-modal-handler }`,
+  templateUrl: './modal-handler.component.html'
+})
+export class ModalHandlerComponent {
+  @Input() modal: ModalComponent;
+
+  @HostListener('click', ['$event'])
+  onClick() {
+    if (this.modal) {
+      this.modal.open();
+    }
+  }
+}
