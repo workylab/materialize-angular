a83118351f768813d13a7d2aa6de74f029a81a9c
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-07-31 14:52:03 -0400

    Refactored collapsible content component.

--- src/app/completed-components/collapsible-content/collapsible-content.component.ts
+++ src/app/completed-components/collapsible-content/collapsible-content.component.ts
@@ -1,32 +1,9 @@
-import { Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/core';
-import { CollapsibleComponent } from '../collapsible/collapsible.component';
-import { CollapsibleContentModel } from './collapsible-content.model';
+import { Component } from '@angular/core';
 import { config } from '../../config';
 
 @Component({
   selector: `${ config.components.prefix }-collapsible-content }`,
   templateUrl: './collapsible-content.component.html'
 })
-export class CollapsibleContentComponent implements CollapsibleContentModel {
-  static readonly defaultProps: CollapsibleContentModel = {
-    className: ''
-  };
-
-  @ViewChild('container', { static: true }) containerRef: ElementRef;
-
-  @Input() className: string = CollapsibleComponent.defaultProps.className;
-
-  public prefix = config.components.prefix;
-
-  constructor(private renderer: Renderer2) {
-  }
-
-  toggle(isOpen: boolean) {
-    const contentContainer: HTMLElement = this.containerRef.nativeElement;
-    const maxHeight = isOpen
-      ? `${ contentContainer.scrollHeight }px`
-      : null;
-
-    this.renderer.setStyle(contentContainer, 'maxHeight', maxHeight);
-  }
+export class CollapsibleContentComponent {
 }
