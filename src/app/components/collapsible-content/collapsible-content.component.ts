4cf4ca6c434bdd67c2950c46cb76ed5f040e67d6
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-07-31 01:22:01 -0400

    Moved collapsible content component.

--- src/app/components/collapsible-content/collapsible-content.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { config } from '../../config';
-
-@Component({
-  selector: `${ config.components.prefix }-collapsible-content }`,
-  templateUrl: './collapsible-content.component.html'
-})
-export class CollapsibleContentComponent {
-}
