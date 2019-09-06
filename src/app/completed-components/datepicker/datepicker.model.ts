10be5f250e1def13ed08bcfcc752f9072c4cc67d
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-09-05 22:11:15 -0400

    Improved date picker model with calendar.

--- src/app/completed-components/datepicker/datepicker.model.ts
+++ src/app/completed-components/datepicker/datepicker.model.ts
@@ -1,6 +1,7 @@
 import { InputModel } from '../input/input.model';
+import { CalendarModel } from '../calendar/calendar.model';
 
-export interface DatePickerModel extends InputModel {
+export interface DatePickerModel extends InputModel, CalendarModel {
   date: Date;
   format: string;
   fullSize: boolean;
