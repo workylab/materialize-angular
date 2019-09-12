a4a0ce5fb66054ee5cd96894b4f4a3fe1de26915
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-09-12 10:07:56 -0400

    Added size parameter to icon component.

--- src/app/completed-components/icon/icon.component.ts
+++ src/app/completed-components/icon/icon.component.ts
@@ -1,18 +1,22 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ICON_SIZES, IconModel } from './icon.model';
 import { config } from '../../config';
-import { IconModel } from './icon.model';
 
 @Component({
   selector: `${ config.components.prefix }-icon }`,
   templateUrl: './icon.component.html'
 })
 export class IconComponent implements IconModel {
-  static readonly defaultProps: IconModel = { className: '' };
+  static readonly defaultProps: IconModel = {
+    className: '',
+    size: ICON_SIZES.SM
+  };
 
   @Output('onClick') onClickEmitter: EventEmitter<void>;
   @Output('onBlur') onBlurEmitter: EventEmitter<void>;
 
   @Input() className: string = IconComponent.defaultProps.className;
+  @Input() size: ICON_SIZES = IconComponent.defaultProps.size;
 
   public prefix = config.components.prefix;
 
