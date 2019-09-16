7df762340e40669fc8cce256cf0a4fe96745c153
Aleajndro Villanueva <ing.alejandrovillanueva@gmail.com>
2019-09-16 01:11:49 -0400

    Added space top on glossary model.

--- src/app/completed-components/glossary/glossary.model.ts
+++ src/app/completed-components/glossary/glossary.model.ts
@@ -3,4 +3,5 @@ import { ScrollSpyComponent } from '../scroll-spy/scroll-spy.component';
 export interface GlossaryModel {
   className: string;
   scrollSpy: ScrollSpyComponent | null;
+  topSpace: number;
 }
