043ca1d2c0f4c7658bca147f6f4bc1102eeb1e5b
Israel Gayoso Perez <igayoso@gmail.com>
2019-09-24 23:49:25 -0400

    Add license script and license header

---
+++ scripts/license.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ..
+
+for file in `find . -not -path "./node_modules/*" -not -path "./dist/*" -name '*.ts'`
+do
+  echo $file
+  if ! grep -q Copyright $file
+  then
+    cat license-header $file >$file.new && mv $file.new $file
+  fi
+done
